2cbe8fbf47b4e5e34ff1d83df2fca4556fc76855
Matteo Ferrini <mimmi.ferrini@gmail.com>
2025-03-30 15:51:29 +0200

    patch: Fix default footer

--- main.go
+++ main.go
@@ -203,7 +203,7 @@ func main() {
     footer := tview.NewTextView()
     footer.SetTextAlign(tview.AlignCenter)
     footer.SetBorder(true)
-    footer.SetText("CTRL-N New • CTRL-S Save")
+    footer.SetText("CTRL-N New • CTRL-S Save • CTRL-P Sync")
 
     grid := tview.NewGrid()
     grid.SetRows(3, 0, 0, 0, 0, 0, 3)
